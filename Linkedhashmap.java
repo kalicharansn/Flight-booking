248bc03c5d39b1f16ce5f3a7e2a916c7b5d94ba1
kalicharansn <49032356+kalicharansn@users.noreply.github.com>
2019-03-28 20:31:41 +0530

    Test data updated and null value removed

--- Linkedhashmap.java
+++ Linkedhashmap.java
@@ -9,11 +9,9 @@ public class Linkedhashmap {
 		
 		// key value pair, key unique, value not unique, hashing is used, only 1 null key, insertion order, null possible
 		LinkedHashMap<Integer, String> empdetails = new LinkedHashMap<Integer, String>();
-		empdetails.put(1, "Arul");
-		empdetails.put(2, "Samy");
-		empdetails.put(3, "Guru");
-		empdetails.put(null, "MS");
-		empdetails.put(null, "Virat");
+		empdetails.put(1, "Ashru");
+		empdetails.put(2, "Isha");
+		empdetails.put(3, "Faizi");
 		System.out.println(empdetails);
 
 		HashMap<Integer, String> empdetailsall = new HashMap<Integer, String>();
